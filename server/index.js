88b504e0d60e3fc6065f1d9a6f7faff5f04491b3
Concross <cacrossley22@gmail.com>
2018-09-24 23:35:29 -0700

    server running

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 'use strict';
+// import app from './src/app';
+const app = require('./src/app');
+
+require('dotenv').config();
+
+require('babel-register')({
+  presets: ['env'],
+});
+
+console.log(process.env.PORT);
+app.start(process.env.PORT);
--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// import express from 'express';
+const express = require('express');
+// import cors from 'cors';
+const cors = require('cors');
+
+let app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let isRunning = false;
+
+app.start = (port) => {
+  if (isRunning) {
+    console.log(`Server is already running on port ${port}`);
+
+  } else {
+    app.listen(port, err => {
+      if (err) { throw err; }
+      console.log(`Server is running on port ${port}`);
+      isRunning = true;
+    });
+  }
+};
+
+app.stop = () => {
+  app.close(() => {
+    isRunning = false;
+    console.log(`Server has been shut down`);
+  });
+};
+
+module.exports = app;
